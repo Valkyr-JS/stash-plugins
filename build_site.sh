10bfcca3164015a7f0dba2c5fc4a1bd55fdcef55
ValkyrJS <valkyr.photos@gmail.com>
2024-06-17 18:52:52 +0100

    Removed theme build

--- build_site.sh
+++ build_site.sh
@@ -74,6 +74,3 @@ buildPlugin()
 find ./plugins -mindepth 1 -name *.yml | while read file; do
     buildPlugin "$file"
 done
-find ./themes -mindepth 1 -name *.yml | while read file; do
-    buildPlugin "$file"
-done
